6ebe48c013596408a2ddbed650748587e9fd31e2
Dariusz Izak <dariusz.izak@ibb.waw.pl>
2019-08-13 11:28:48 +0200

    Remove citedby k-val since it updates too frequently

--- tests/tests.py
+++ tests/tests.py
@@ -22,8 +22,10 @@ class ResponseTests(unittest.TestCase):
 
         with open('test_data/ResponseTests/ref_pawelsiedlecki.json') as fin:
             self.ref_resp = json.loads(fin.read())
+            del self.ref_resp['citedby']
         with open('test_data/ResponseTests/ref_pawelsiedlecki_ibb.json') as fin:
             self.ref_resp_author_affiliation = json.loads(fin.read())
+            del self.ref_resp_author_affiliation['citedby']
 
     def test_response_author(self):
         """
@@ -32,6 +34,7 @@ class ResponseTests(unittest.TestCase):
         self.test_resp = json.loads(
             str(self.client.get('/author/pawelsiedlecki').data, 'utf-8'),
         )
+        del self.test_resp['citedby']
         self.assertDictEqual(self.ref_resp, self.test_resp)
 
     def test_response_author_affiliation(self):
@@ -44,4 +47,5 @@ class ResponseTests(unittest.TestCase):
                 'utf-8',
             ),
         )
+        del self.test_resp['citedby']
         self.assertDictEqual(self.ref_resp_author_affiliation, self.test_resp)

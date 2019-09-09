5a72a639fe7d5c6ddb6e77026022462dcf0361b6
Dariusz Izak <dariusz.izak@ibb.waw.pl>
2019-09-09 11:58:06 +0200

    Enh#9 (#12)
    * import modules for storing cache serializing to/from json and calculating timedelta
    
    * Connect to redis
    
    * Read from cache if key exists
    
    * Store cleaned search in var used to serilization and converting to response
    
    * Store search in cache and return response
    
    * Add redis service for storing cache
    
    * Add redis to required packages
    
    * Removed trailing whitespice
    
    * Set cache to None if the connection fails
    
    * Conditional cache use only if it exists
    
    * Conditional cache use only if it exists. Line break formatting
    
    * Refer to first_result_json only if cache exists

--- flascholarly/app.py
+++ flascholarly/app.py
@@ -1,5 +1,5 @@
 """
-This is the microservice-wrapper around scholarly module authored by 
+This is the microservice-wrapper around scholarly module authored by
 Steven A. Cholewniak. Scholarly is a Google Scholar scraper. The microservice
 serves mainly for getting the scholar information with plain javascript in
 static websites.
@@ -9,10 +9,21 @@ static websites.
 from flask import Flask
 from flask import jsonify
 from flask_cors import CORS
+import redis
+import json
+import datetime
 import scholarly as sch
 
 app = Flask(__name__)
 CORS(app)
+
+cache = redis.StrictRedis(db=0, host='redis-db', port=6379)
+
+try:
+    cache.info()
+except redis.exceptions.ConnectionError:
+    cache = None
+
 
 @app.route('/author/<string:author>', defaults={'affiliation': None})
 @app.route('/author/<string:author>/affiliation/<string:affiliation>')
@@ -20,17 +31,34 @@ def search(
     author,
     affiliation,
 ):
+    if cache:
+        first_result_json = cache.get('{}+{}'.format(
+            author,
+            affiliation,
+        ))
+        if first_result_json:
+            return first_result_json
     query = sch.search_author(
         ', '.join((i for i in (author, affiliation) if i))
     )
     first_result = next(query)
-    return jsonify({
+    first_result_dict = {
         'name': first_result.name,
         'affiliation': first_result.affiliation,
         'citedby': first_result.citedby,
         'interests': first_result.interests,
         'url_picture': first_result.url_picture,
-    })
+    }
+    if cache:
+        cache.set(
+            '{}+{}'.format(author, affiliation),
+            json.dumps(first_result_dict),
+        )
+        cache.pexpire(
+            '{}+{}'.format(author, affiliation),
+            datetime.timedelta(days=1),
+        )
+    return jsonify(first_result_dict)
 
 
 if __name__ == '__main__':
